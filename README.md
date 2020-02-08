3e008356de4388b1445866b3739fdddb619f2740
Jeremy Morgan <jeremymorgan@gmail.com>
2020-02-08 10:24:45 -0800

    Initial Commit

--- README.md
+++ README.md
@@ -1,2 +1,32 @@
-# FindTheBadGuys
-Get locations (Continent, Country, City, Subdivision) from a text list of IP addresses
+# Find The Bad Guys
+
+This is a simple GoLang app that gets the locations (Continent, Country, City, Subdivision) from a text list of IP addresses you provide. 
+
+It uses the [Maxmind GeoLite Database](https://dev.maxmind.com/geoip/geoip2/geolite2/) but I did not include it as I don't have the right to redistribute it. 
+
+You can get your own GeoIP account and get the database free of charge [by clicking here](https://www.maxmind.com/en/geolite2/signup).
+
+It also uses this [Unofficial MaxMind GeoIP2 Reader for Go](https://github.com/oschwald/geoip2-golang)
+
+## Steps:
+
+Start with a text file with a list of IP addresses. If you are running CentOS you can [use this script](https://gist.github.com/JeremyMorgan/94af88899785ea725a55a382f3fd209b)
+
+Download the Geolite database. Add its location to the **geolitedb** variable
+
+Add the location of the IP address file to the **badguysfile** variable
+
+Run **iplookup**
+
+It will store the following files:
+
+continents.txt - a list of continents
+countries.txt - a list of countries
+cities.txt - a list of cities
+subdivisions.txt - a list of subdivisions of city
+
+
+They are not checked for duplicates, in case you want to do counts on each. 
+
+If this application is useful, [let me know](https://twitter.com/JeremyCMorgan) and I'll update it and add features. 
+
---
+++ iplookup.go
@@ -0,0 +1,117 @@
+package main
+
+import (
+	"bufio"
+	"bytes"
+	"github.com/oschwald/geoip2-golang"
+	"io/ioutil"
+	"log"
+	"net"
+	"os"
+)
+
+func main() {
+
+	// your bad guys file, list of all IP addresses of attackers
+	// You can get this from centos with this script : https://gist.github.com/JeremyMorgan/94af88899785ea725a55a382f3fd209b
+	var badguysfile = "[Your path to IP addresses]"
+
+	// location of your geoliteDB file
+	var geolitedb = "[Your path to GeoLite2-City.mmdb]"
+
+	// files you want to store your data in:
+	var contintentsFile = "continents.txt"
+	var countriesFile = "countries.txt"
+	var citiesFile = "cities.txt"
+	var subdivisionsFile = "subdivisions.txt"
+
+	/////////////    No need to edit past this line  ///////////////
+
+	// create some buffers for our data
+	var continents bytes.Buffer
+    var countries bytes.Buffer
+	var cities bytes.Buffer
+	var subdivisions bytes.Buffer
+
+	// open the GeoLite2 database
+	db, err := geoip2.Open(geolitedb)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+
+	// open up bad guys file (list of IP addresses from attackers
+	file, err := os.Open(badguysfile)
+
+	if err != nil {
+		log.Fatalf("We couldn't open your bad guys file: %s", err)
+	}
+
+	// parse it line by line
+	scanner := bufio.NewScanner(file)
+	scanner.Split(bufio.ScanLines)
+
+	var ipaddresses []string
+
+	for scanner.Scan() {
+		ipaddresses = append(ipaddresses, scanner.Text())
+	}
+
+	// close the file, we don't need it anymore
+	file.Close()
+
+	// loop through our IP addresses and make a lookup to our database for each
+	for _, ipaddress := range ipaddresses {
+
+		ip := net.ParseIP(ipaddress)
+		record, err := db.City(ip)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		// write continent name into buffer
+		if len(record.Continent.Names["en"]) > 0 {
+			continents.WriteString(record.Continent.Names["en"] + "\n")
+		}
+
+		// write country name into buffer
+		if len(record.Country.Names["en"]) > 0 {
+			countries.WriteString(record.Country.Names["en"] + "\n")
+		}
+
+		// write city name into buffer
+		if len(record.City.Names["en"]) > 0 {
+			cities.WriteString(record.City.Names["en"] + "\n")
+		}
+
+		// write subdivision into buffer
+		if len(record.Subdivisions) > 0 {
+			subdivisions.WriteString(record.Subdivisions[0].Names["en"] + "\n")
+		}
+	}
+
+	// write the continents file
+	err = ioutil.WriteFile(contintentsFile, continents.Bytes(), 0644)
+	if err != nil {
+		panic(err)
+	}
+
+	// write the countries file
+	err = ioutil.WriteFile(countriesFile, countries.Bytes(), 0644)
+	if err != nil {
+		panic(err)
+	}
+
+	// write the cities file
+	err = ioutil.WriteFile(citiesFile, cities.Bytes(), 0644)
+	if err != nil {
+		panic(err)
+	}
+
+	// write the subdivisions file
+	err = ioutil.WriteFile(subdivisionsFile, subdivisions.Bytes(), 0644)
+	if err != nil {
+		panic(err)
+	}
+}
